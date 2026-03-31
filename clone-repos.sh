541fdac4f67eb901ed979a0e260f860e5107ffe2
lanijw <github.com.chomp570@passmail.com>
2026-03-31 22:31:15 +0200

    Disables getting part of path for folder name.

--- clone-repos.sh
+++ clone-repos.sh
@@ -20,16 +20,7 @@ while IFS= read -r url || [[ -n "$url" ]]; do
   url="${url//$'\r'/}"
   [[ -z "$url" || "$url" == \#* ]] && continue
 
-  # Extract the text between the 5th and 6th slash
-  # URLs have 2 slashes in scheme (https://), so field 6 = after domain, field 7 = repo name
-  folder=$(echo "$url" | cut -d'/' -f6-)
-
-  if [[ -z "$folder" ]]; then
-    echo "Warning: could not parse folder name from '$url', skipping"
-    continue
-  fi
-
-  echo "Cloning '$url' into './$folder' ..."
-  git clone "$url" "$folder"
+  echo "Cloning '$url' ..."
+  git clone "$url"
 done < "$file"
 
